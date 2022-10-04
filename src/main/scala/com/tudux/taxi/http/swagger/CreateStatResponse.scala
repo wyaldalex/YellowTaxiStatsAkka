e000717e6906a05996b79561f3f8795c1e0e05a5
AlexAR <moltron_nano@hotmail.com>
2022-10-04 10:56:18 -0600

    feat: improve naming for create stat swagger definition

--- src/main/scala/com/tudux/taxi/http/swagger/CreateStatResponse.scala
+++ src/main/scala/com/tudux/taxi/http/swagger/CreateStatResponse.scala
@@ -6,18 +6,19 @@ import com.tudux.taxi.http.CreateTaxiStatRequest
 import io.swagger.annotations._
 import io.swagger.models.Operation
 
-@Path("/api/yellowtaxi")
+@Path("/api/yellowtaxi/stat")
 @Api(value = "/stat")
 @SwaggerDefinition(tags = Array(new Tag(name = "CreateTaxiTripStat", description = "Operation used to create Taxi Trip Stat")))
 trait CreateStatResponse {
   @Operation
   @ApiOperation(value = "stat", tags = Array("stat"), httpMethod = "POST", notes = "This route will return the id of the created stat"   )
   @ApiImplicitParams(Array(
-    new ApiImplicitParam(name = "CreateStatRequest", required = true,
+    new ApiImplicitParam(name = "CreateTaxiStatRequest", required = true,
       dataTypeClass = classOf[CreateTaxiStatRequest] , paramType = "body")
   ))
   @ApiResponses(Array(
-    new ApiResponse(code = 200, message = "OK"),
+    new ApiResponse(code = 200, message = "Stat created with Id {newStatId}"),
+    new ApiResponse(code = 400, message = "The request content was malformed"),
     new ApiResponse(code = 500, message = "There was an internal server error.")
   ))
   def pingSwagger: Option[Route] = None

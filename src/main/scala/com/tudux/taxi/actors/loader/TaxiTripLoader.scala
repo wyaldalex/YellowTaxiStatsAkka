002c14306a021c8cdd0d98f8fa5e026674395344
AlexAR <moltron_nano@hotmail.com>
2022-10-28 16:40:35 -0600

    feat: loader changes

--- src/main/scala/com/tudux/taxi/actors/loader/TaxiTripLoader.scala
+++ src/main/scala/com/tudux/taxi/actors/loader/TaxiTripLoader.scala
@@ -3,14 +3,20 @@ package com.tudux.taxi.actors.loader
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 import akka.util.Timeout
 import com.tudux.taxi.actors.aggregators.{PersistentCostStatsAggregator, PersistentTimeStatsAggregator}
+import com.tudux.taxi.actors.common.response.CommonOperationResponse.OperationResponse
+import com.tudux.taxi.actors.cost.TaxiTripCostCommand.CreateTaxiTripCost
+import com.tudux.taxi.actors.extrainfo.TaxiTripExtraInfoCommand.CreateTaxiTripExtraInfo
 import com.tudux.taxi.actors.implicits.TaxiTripImplicits._
+import com.tudux.taxi.actors.passenger.TaxiTripPassengerInfoCommand.CreateTaxiTripPassengerInfo
+import com.tudux.taxi.actors.timeinfo.TaxiTripTimeInfoCommand.CreateTaxiTripTimeInfo
 import com.tudux.taxi.app.ShardedActorsGenerator.{createShardedCostActor, createShardedExtraInfoActor, createShardedPassengerInfoActor, createShardedTimeInfoActor}
 import com.typesafe.config.ConfigFactory
 
 import java.util.UUID
-import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
 import scala.io.Source
+import scala.util.{Failure, Success}
 
 
 case class TaxiTripEntry(vendorID: Int, tpepPickupDatetime: String, tpepDropoffDatetime: String, passengerCount: Int,
@@ -22,8 +28,8 @@ case class TaxiTripEntry(vendorID: Int, tpepPickupDatetime: String, tpepDropoffD
 sealed trait TaxiTripCommand
 object TaxiTripCommand {
   case class CreateTaxiTripCommand(taxiTrip: TaxiTripEntry, tripId: String = "") extends  TaxiTripCommand
-  case class CreateTaxiTrip(taxiTrip: TaxiTripEntry) extends TaxiTripCommand
-  case class DeleteTaxiTrip(tripId: String) extends TaxiTripCommand
+
+
 }
 sealed trait TaxiTripResponse
 object TaxiTripResponse {
@@ -33,20 +39,15 @@ object TaxiTripResponse {
 
 object TaxiTripActor {
   def props(parentCostShardedActor: ActorRef,parentExtraInfoShardedActor: ActorRef,
-            parentPassengerShardedActor: ActorRef,parentTimeShardedActor: ActorRef,
-            costAggregatorActor : ActorRef, timeAggregatorActor : ActorRef): Props =
+            parentPassengerShardedActor: ActorRef,parentTimeShardedActor: ActorRef): Props =
     Props(new TaxiTripActor(parentCostShardedActor,parentExtraInfoShardedActor,
-      parentPassengerShardedActor, parentTimeShardedActor, costAggregatorActor, timeAggregatorActor))
+      parentPassengerShardedActor, parentTimeShardedActor))
 
 }
 class TaxiTripActor(parentCostShardedActor: ActorRef,parentExtraInfoShardedActor: ActorRef,
-                    parentPassengerShardedActor: ActorRef,parentTimeShardedActor: ActorRef,
-  costAggregatorActor : ActorRef, timeAggregatorActor : ActorRef) extends Actor with ActorLogging {
+                    parentPassengerShardedActor: ActorRef,parentTimeShardedActor: ActorRef) extends Actor with ActorLogging {
 
   import TaxiTripCommand._
-  import TaxiTripResponse._
-  import com.tudux.taxi.actors.aggregators.CostAggregatorCommand._
-  import com.tudux.taxi.actors.aggregators.TimeAggregatorCommand._
   import com.tudux.taxi.actors.cost.TaxiTripCostCommand._
   import com.tudux.taxi.actors.extrainfo.TaxiTripExtraInfoCommand._
   import com.tudux.taxi.actors.passenger.TaxiTripPassengerInfoCommand._
@@ -57,14 +58,18 @@ class TaxiTripActor(parentCostShardedActor: ActorRef,parentExtraInfoShardedActor
       //generate new stat ID to avoid conflicts
       val tripId = UUID.randomUUID().toString
       log.info(s"Received $taxiTrip to create")
+      import akka.pattern.{ask, pipe}
+      implicit val ec: ExecutionContext = context.dispatcher
+      implicit val timeout: Timeout = 5.seconds
 
-      parentCostShardedActor ! CreateTaxiTripCost(tripId,taxiTrip)
-      parentExtraInfoShardedActor ! CreateTaxiTripExtraInfo(tripId,taxiTrip)
-      parentPassengerShardedActor ! CreateTaxiTripPassengerInfo(tripId,taxiTrip)
-      parentTimeShardedActor ! CreateTaxiTripTimeInfo(tripId,taxiTrip)
+      val costFuture = (parentCostShardedActor ? CreateTaxiTripCost(tripId,taxiTrip))
+      val extraInfoFuture = (parentExtraInfoShardedActor ? CreateTaxiTripExtraInfo(tripId,taxiTrip))
+      val passengerFuture = (parentPassengerShardedActor ? CreateTaxiTripPassengerInfo(tripId,taxiTrip))
+      val timeFuture = (parentTimeShardedActor ? CreateTaxiTripTimeInfo(tripId,taxiTrip))
 
-      sender() ! TaxiTripCreatedResponse(tripId)
-
+      //sender() ! TaxiTripCreatedResponse(tripId)
+      //Future(costFuture,extraInfoFuture,passengerFuture,timeFuture).pipeTo(sender())
+      sender ! (costFuture,extraInfoFuture,passengerFuture,timeFuture)
     case printTimeToLoad@PrintTimeToLoad(_) =>
       log.info("Forwarding Total Time to Load Request")
 //      taxiTripCostActor.forward(printTimeToLoad)
@@ -85,7 +90,7 @@ object TaxiStatAppLoader extends App {
 """.stripMargin).withFallback(ConfigFactory.load("sharded/shardedConfigSettings.conf"))
 
   val system: ActorSystem = ActorSystem("YellowTaxiCluster", config)
-  implicit val timeout: Timeout = Timeout(2.seconds)
+  implicit val timeout: Timeout = Timeout(10.seconds)
   implicit val scheduler: ExecutionContext = system.dispatcher
   /*
    val localStoreActorSystem = ActorSystem("cassandraSystem", ConfigFactory.load().getConfig("cassandraDemo"))
@@ -101,7 +106,7 @@ object TaxiStatAppLoader extends App {
   val parentTimeInfoShardRegionRef: ActorRef = createShardedTimeInfoActor(system,timeAggregatorActor)
 
   val taxiTripActor = system.actorOf(TaxiTripActor.props(parentCostShardRegionRef,parentExtraInfoShardRegionRef,
-    parentPassengerShardRegionRef,parentTimeInfoShardRegionRef,costAggregatorActor,timeAggregatorActor), "parentTaxiActor")
+    parentPassengerShardRegionRef,parentTimeInfoShardRegionRef), "parentTaxiActor")
 
   import kantan.csv._
   import kantan.csv.ops._ // Automatic derivation of codecs.
@@ -118,51 +123,35 @@ object TaxiStatAppLoader extends App {
   //Give time for cluster to start up
   Thread.sleep(60000)
   import TaxiTripCommand._
-  import com.tudux.taxi.actors.cost.TaxiTripCostCommand._
   //Data loading:
+//  reader.foreach(either => {
+//    taxiTripActor ! CreateTaxiTripCommand((either.right.getOrElse(TaxiTripEntry(
+//      2,"2015-01-15 19:05:39","2015-01-15 19:23:42",1,1.59,-73.993896484375,40.750110626220703,1,"N",-73.974784851074219,40.750617980957031,1,12,1,0.5,3.25,0,0.3,17.05
+//    ))))
+//  })
+  var loaderCounter = 1
+  import akka.pattern.ask
   reader.foreach(either => {
-    taxiTripActor ! CreateTaxiTripCommand((either.right.getOrElse(TaxiTripEntry(
-      2,"2015-01-15 19:05:39","2015-01-15 19:23:42",1,1.59,-73.993896484375,40.750110626220703,1,"N",-73.974784851074219,40.750617980957031,1,12,1,0.5,3.25,0,0.3,17.05
-    ))))
+    val tripId = UUID.randomUUID().toString
+    println(s"Persisting Id $tripId")
+    val costResponse = (parentCostShardRegionRef ? CreateTaxiTripCost(tripId, either.right.get)).mapTo[OperationResponse]
+    val extraInfoResponse = (parentExtraInfoShardRegionRef ? CreateTaxiTripExtraInfo(tripId, either.right.get)).mapTo[OperationResponse]
+    val passengerResponse = (parentPassengerShardRegionRef ? CreateTaxiTripPassengerInfo(tripId, either.right.get)).mapTo[OperationResponse]
+    val timeResponse = (parentTimeInfoShardRegionRef ? CreateTaxiTripTimeInfo(tripId, either.right.get)).mapTo[OperationResponse]
+    val combineResponse = for {
+      r1 <- costResponse
+      r2 <- extraInfoResponse
+      r3 <- passengerResponse
+      r4 <- timeResponse
+    } yield (r1, r2, r3, r4)
+    println(s"Combined response $combineResponse")
+    combineResponse.onComplete{
+      case Success(value) =>
+        println(s"Loader response $value for entry $loaderCounter and id $tripId" )
+        loaderCounter += 1
+      case Failure(exception) =>
+        println(exception.getMessage)
+    }
   })
 
-  //Operations testing
-
-  taxiTripActor ! PrintTimeToLoad(startTimeMillis)
-  //cb17c9a7-31d1-4863-ab1f-11bb7ed115f5
-  taxiTripActor ! GetTaxiTripCost("2d4c8a0d-948f-4d69-a438-57c54abb2a84")
-
-
-
-
-
-
-  /*
-  reader.foreach(either => {
-    persistentTaxiStatActor ! CreateTaxiStat((either.right.getOrElse(1)))
-  }) */
-
-  /*
-   val quickListTest : List[String] = List(
-     "2,2015-01-15 19:05:39,2015-01-15 19:23:42,1,1.59,-73.993896484375,40.750110626220703,1,N,-73.974784851074219,40.750617980957031,1,12,1,0.5,3.25,0,0.3,17.05",
-     "1,2015-01-10 20:33:38,2015-01-10 20:53:28,1,3.30,-74.00164794921875,40.7242431640625,1,N,-73.994415283203125,40.759109497070313,1,14.5,0.5,0.5,2,0,0.3,17.8",
-     "1,2015-01-10 20:33:38,2015-01-10 20:43:41,1,1.80,-73.963340759277344,40.802787780761719,1,N,-73.951820373535156,40.824413299560547,2,9.5,0.5,0.5,0,0,0.3,10.8",
-     "1,2015-01-10 20:33:39,2015-01-10 20:35:31,1,.50,-74.009086608886719,40.713817596435547,1,N,-74.004325866699219,40.719985961914063,2,3.5,0.5,0.5,0,0,0.3,4.8",
-     "1,2015-01-10 20:33:39,2015-01-10 20:52:58,1,3.00,-73.971176147460938,40.762428283691406,1,N,-74.004180908203125,40.742652893066406,2,15,0.5,0.5,0,0,0.3,16.3",
-     "1,2015-01-10 20:33:39,2015-01-10 20:53:52,1,9.00,-73.874374389648438,40.7740478515625,1,N,-73.986976623535156,40.758193969726563,1,27,0.5,0.5,6.7,5.33,0.3,40.33",
-     "1,2015-01-10 20:33:39,2015-01-10 20:58:31,1,2.20,-73.9832763671875,40.726009368896484,1,N,-73.992469787597656,40.7496337890625,2,14,0.5,0.5,0,0,0.3,15.3",
-   )*/
-
-
-
-}
-
-object RandomTest extends App {
-  val format = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:SS")
-  val d1 = format.parse("2015-01-10 20:58:31")
-  val d2 = format.parse("2015-01-10 21:58:31")
-  println(d1)
-  println(d2)
-
-  println((d2.getTime - d1.getTime)/60000)
 }

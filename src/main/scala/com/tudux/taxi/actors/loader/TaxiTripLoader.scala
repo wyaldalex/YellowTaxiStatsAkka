468ba9c20b598f99623218e9aa835e0c26a38848
AlexAR <moltron_nano@hotmail.com>
2022-11-07 13:58:38 -0600

    feat: make graphDSL loader the only loader and add counters

--- src/main/scala/com/tudux/taxi/actors/loader/TaxiTripLoader.scala
+++ src/main/scala/com/tudux/taxi/actors/loader/TaxiTripLoader.scala
@@ -3,6 +3,7 @@ package com.tudux.taxi.actors.loader
 import akka.NotUsed
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 import akka.pattern.ask
+import akka.stream.ClosedShape
 import akka.stream.scaladsl.{Broadcast, FileIO, Flow, Framing, GraphDSL, RunnableGraph, Sink}
 import akka.util.{ByteString, Timeout}
 import com.tudux.taxi.actors.aggregators.{PersistentCostStatsAggregator, PersistentTimeStatsAggregator}
@@ -14,14 +15,10 @@ import com.tudux.taxi.actors.passenger.TaxiTripPassengerInfoCommand.CreateTaxiTr
 import com.tudux.taxi.actors.timeinfo.TaxiTripTimeInfoCommand.CreateTaxiTripTimeInfo
 import com.tudux.taxi.app.ShardedActorsGenerator.{createShardedCostActor, createShardedExtraInfoActor, createShardedPassengerInfoActor, createShardedTimeInfoActor}
 import com.typesafe.config.ConfigFactory
-import akka.stream.{ActorMaterializer, ClosedShape, OverflowStrategy}
 
 import java.nio.file.Paths
-import java.util.UUID
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
-import scala.io.Source
-import scala.util.{Failure, Success}
 
 case class TaxiTripEntry(vendorID: Int, tpepPickupDatetime: String, tpepDropoffDatetime: String, passengerCount: Int,
                          tripDistance: Double, pickupLongitude: Double, pickupLatitude: Double, rateCodeID: Int,
@@ -34,80 +31,18 @@ object TaxiTripCommand {
   case class CreateTaxiTripCommand(taxiTrip: TaxiTripEntry, tripId: String = "") extends  TaxiTripCommand
 }
 
-object TaxiStatAppLoader extends App {
-  // testing sharding of cost actor
-  val config = ConfigFactory.parseString(
-    "akka.remote.artery.canonical.port = 2551".stripMargin).withFallback(ConfigFactory.load("sharded/shardedConfigSettings.conf"))
-
-  val system: ActorSystem = ActorSystem("YellowTaxiCluster", config)
-  implicit val timeout: Timeout = Timeout(10.seconds)
-  implicit val scheduler: ExecutionContext = system.dispatcher
-
-  val costAggregatorActor: ActorRef = system.actorOf(PersistentCostStatsAggregator.props("cost-aggregator"), "cost-aggregator")
-  val timeAggregatorActor: ActorRef = system.actorOf(PersistentTimeStatsAggregator.props("time-aggregator"), "time-aggregator")
-
-  val parentCostShardRegionRef: ActorRef = createShardedCostActor(system,costAggregatorActor)
-  val parentExtraInfoShardRegionRef: ActorRef = createShardedExtraInfoActor(system)
-  val parentPassengerShardRegionRef: ActorRef = createShardedPassengerInfoActor(system)
-  val parentTimeInfoShardRegionRef: ActorRef = createShardedTimeInfoActor(system,timeAggregatorActor)
-
-  import kantan.csv._
-  import kantan.csv.ops._ //  Automatic derivation of codecs.
-  implicit val decoder: RowDecoder[TaxiTripEntry] = RowDecoder.ordered(TaxiTripEntry.apply _)
-  // Start Processing including reading of the file
-  val startTimeMillis = System.currentTimeMillis()
-  //val source_csv = Source.fromResource("smallset.csv").mkString
-  // val source_csv = Source.fromResource("100ksample.csv").mkString
-   val source_csv = Source.fromResource("1ksample.csv").mkString
-  // val source_csv = Source.fromResource("10ksample.csv").mkString
-  // val source_csv = Source.fromResource("1millSample.csv").mkString
-  val reader = source_csv.asCsvReader[TaxiTripEntry](rfc)
-  // TODO: More robust loader, buisiness need
-
-  object ActorCounter {
-    def props: Props = Props(new ActorCounter)
-    case class IncreaseCount(operationResponse: (OperationResponse,OperationResponse,OperationResponse,OperationResponse))
-  }
-  class ActorCounter extends Actor with ActorLogging {
-    import ActorCounter._
-    var counter = 1
-    override def receive: Receive = {
-      case IncreaseCount(operationResponse) =>
-        log.info(s"TaxiTrip Info Loaded Number: $counter with values ${operationResponse.toString}")
-        counter += 1
-    }
-  }
-  val actorCounter = system.actorOf(ActorCounter.props)
-
-  import ActorCounter._
-  import akka.pattern.ask
-
-  reader.foreach(either => {
-    val tripId = UUID.randomUUID().toString
-    println(s"Persisting Id $tripId")
-    // Scapegoat related refactoring
-    val defaultTaxiEntryVal = TaxiTripEntry(0, "", "", 0, 0, 0, 0, 0, "", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-    val defaultVal: TaxiTripEntry = either.right.getOrElse(defaultTaxiEntryVal)
-
-    val costResponse = (parentCostShardRegionRef ? CreateTaxiTripCost(tripId,defaultVal)).mapTo[OperationResponse]
-    val extraInfoResponse = (parentExtraInfoShardRegionRef ? CreateTaxiTripExtraInfo(tripId, defaultVal)).mapTo[OperationResponse]
-    val passengerResponse = (parentPassengerShardRegionRef ? CreateTaxiTripPassengerInfo(tripId, defaultVal)).mapTo[OperationResponse]
-    val timeResponse = (parentTimeInfoShardRegionRef ? CreateTaxiTripTimeInfo(tripId, defaultVal)).mapTo[OperationResponse]
-    val combineResponse = for {
-      r1 <- costResponse
-      r2 <- extraInfoResponse
-      r3 <- passengerResponse
-      r4 <- timeResponse
-    } yield (r1, r2, r3, r4)
-    combineResponse.onComplete{
-      case Success(value) =>
-        actorCounter ! IncreaseCount(value)
-      case Failure(exception) =>
-        println(exception.getMessage)
-    }
-  })
+object ActorCounter {
+  def props(name: String): Props = Props(new ActorCounter(name))
 }
+class ActorCounter(name: String) extends Actor with ActorLogging {
+  var counter = 1
 
+  override def receive: Receive = {
+    case operationResponse: OperationResponse =>
+      log.info(s"Loader Counter $name received processing response: $counter with values ${operationResponse.toString}")
+      counter += 1
+  }
+}
 object TaxiStatAppGraphDslLoader extends App {
 
   import com.tudux.taxi.reader.CSVConversions._
@@ -127,22 +62,24 @@ object TaxiStatAppGraphDslLoader extends App {
   val parentPassengerShardRegionRef: ActorRef = createShardedPassengerInfoActor(system)
   val parentTimeInfoShardRegionRef: ActorRef = createShardedTimeInfoActor(system, timeAggregatorActor)
 
+  val costCounter: ActorRef =  system.actorOf(ActorCounter.props("CostLoaderCounter"))
+  val extraInfoCounter: ActorRef =  system.actorOf(ActorCounter.props("ExtraInfoLoaderCounter"))
+  val passengerCounter: ActorRef =  system.actorOf(ActorCounter.props("PassengerLoaderCounter"))
+  val timeCounter: ActorRef =  system.actorOf(ActorCounter.props("TimeLoaderCounter"))
+
   val file = Paths.get("src/main/resources/1ksample.csv")
   // val file = Paths.get("src/main/resources/10ksample.csv")
 
-  implicit val materializer = ActorMaterializer()
-
-  val fileSource = FileIO
-    .fromPath(file)
+  val fileSource = FileIO.fromPath(file)
 
   val toStringFlow = Framing.delimiter(ByteString("\n"), 256, true).map(_.utf8String)
   val toTaxiEntryFlow = Flow[String].map(fromCsvEntryToCaseClass)
   val addCommonUUIDFlow = Flow[TaxiTripEntry].map(addUUID)
 
-  val costSink = Sink.foreach[OperationResponse](x => println(x.toString()))
-  val extraInfoSink = Sink.foreach[OperationResponse](x => println(x.toString()))
-  val passengerSink = Sink.foreach[OperationResponse](x => println(x.toString()))
-  val timeInfoSink = Sink.foreach[OperationResponse](x => println(x.toString()))
+  val costSink = Sink.foreach[OperationResponse](x => costCounter ! x)
+  val extraInfoSink = Sink.foreach[OperationResponse](x => extraInfoCounter ! x)
+  val passengerSink = Sink.foreach[OperationResponse](x => passengerCounter ! x)
+  val timeInfoSink = Sink.foreach[OperationResponse](x => timeCounter ! x)
 
   val costFlow = Flow[(String,TaxiTripEntry)].mapAsync(parallelism = 1)(event => (parentCostShardRegionRef ? CreateTaxiTripCost(event._1,event._2)).mapTo[OperationResponse]).to(costSink)
   val extraInfoFlow = Flow[(String,TaxiTripEntry)].mapAsync(parallelism = 1)(event => (parentExtraInfoShardRegionRef ?  CreateTaxiTripExtraInfo(event._1,event._2)).mapTo[OperationResponse]).to(extraInfoSink)
@@ -162,5 +99,4 @@ object TaxiStatAppGraphDslLoader extends App {
       ClosedShape
   })
   graph.run()
-
 }

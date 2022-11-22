4f6a54b52c006f311c70ad8be5203814af7548bf
Víctor Gómez <vgomez@applaudostudios.com>
2022-11-22 09:02:21 -0400

    Create file for testing taxiTrip

---
+++ src/test/scala/com/tudux/taxi/http/taxiTripRouterSpec.scala
@@ -0,0 +1,9 @@
+package com.tudux.taxi.http
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import org.scalatest.featurespec.AnyFeatureSpecLike
+import org.scalatest.GivenWhenThen
+
+class taxiTripRouterSpec extends AnyFeatureSpecLike  with GivenWhenThen with SprayJsonSupport{
+
+}

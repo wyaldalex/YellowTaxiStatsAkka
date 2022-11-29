ef32a40efe0bfef3e4db1dc9d68403d2861a5993
Víctor Gómez <vgomez@applaudostudios.com>
2022-11-29 11:42:46 -0400

    Remove unnecessary configuration and move some test

--- src/test/scala/com/tudux/taxi/actors/extrainfo/PersistentTaxiExtraInfoSpec.scala
+++ src/test/scala/com/tudux/taxi/actors/extrainfo/PersistentTaxiExtraInfoSpec.scala
@@ -35,17 +35,20 @@ class PersistentTaxiExtraInfoSpec
 
   Feature("handle recover") {
     Scenario("Recover state after a restart happens") {
-      Given("a PersistentTaxiExtraInfo actor")
+      Given("a PersistentTaxiExtraInfo actor and commands")
       val tripId: String = "SADFASDF2886J865DASJKANDS45676IWQEWFLWF234LQHJW"
       val APersistentTaxiExtraInfoActor = system.actorOf(PersistentTaxiExtraInfo.props)
 
       val taxiExtraInfoStat: TaxiTripExtraInfo = TaxiTripExtraInfo(pickupLongitude = -73.991127014160156,
         pickupLatitude = 40.750080108642578, rateCodeID = 1, storeAndFwdFlag = "N", dropoffLongitude =
-          -73.988609313964844,
-        dropoffLatitude = 40.734889984130859)
+          -73.988609313964844, dropoffLatitude = 40.734889984130859)
       val createCommand = TaxiTripExtraInfoCommand.CreateTaxiTripExtraInfo(tripId, taxiExtraInfoStat)
+      val getCommand = TaxiTripExtraInfoCommand.GetTaxiTripExtraInfo(tripId)
+
       APersistentTaxiExtraInfoActor ! createCommand
       expectMsg(OperationResponse(tripId,Right("Success")))
+      //APersistentTaxiExtraInfoActor ! getCommand
+      //expectMsg(taxiExtraInfoStat)
 
       When("A restart happens")
       APersistentTaxiExtraInfoActor ! Kill
@@ -53,7 +56,6 @@ class PersistentTaxiExtraInfoSpec
       Then("PersistentTaxiExtraInfo should restore his state")
       val ARestartedPersistentTaxiExtraInfoActor = system.actorOf(PersistentTaxiExtraInfo.props)
 
-      val getCommand = TaxiTripExtraInfoCommand.GetTaxiTripExtraInfo(tripId)
       ARestartedPersistentTaxiExtraInfoActor ! getCommand
       expectMsg(taxiExtraInfoStat)
     }

cd9eae3ebcb6ed40dd32a98b1c90b2cd4dbc3d51
Víctor Gómez <vgomez@applaudostudios.com>
2022-11-29 18:32:16 -0400

    Testing all the recover of actors

--- src/test/scala/com/tudux/taxi/actors/cost/PersistentTaxiTripCostSpec.scala
+++ src/test/scala/com/tudux/taxi/actors/cost/PersistentTaxiTripCostSpec.scala
@@ -1,6 +1,6 @@
 package com.tudux.taxi.actors.cost
 
-import akka.actor.{ActorSystem, Kill}
+import akka.actor.{ActorSystem, Kill, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import com.tudux.taxi.actors.aggregators.PersistentCostStatsAggregator
 import com.tudux.taxi.actors.common.response.CommonOperationResponse.OperationResponse
@@ -18,30 +18,56 @@ class PersistentTaxiTripCostSpec extends TestKit(ActorSystem("PersistentTaxiExtr
   Feature("handle recover") {
     Scenario("Recover state after a restart happens") {
       Given("a PersistentTaxiExtraInfo actor")
-      val tripId: String = "SADFASDF2886J865DASJKANDS45676IWQEWFLWF234LQHJW"
-      val APersistentCostStatsAggregatorActor = system.actorOf(PersistentCostStatsAggregator.props
+      // Creating Actor
+      val aPersistentCostStatsAggregatorActor = system.actorOf(PersistentCostStatsAggregator.props
       ("cost-aggregator-test"))
-      val APersistentTaxiTripCostActor = system.actorOf(PersistentTaxiTripCost.props
-      (APersistentCostStatsAggregatorActor))
+      val aPersistentTaxiTripCostActor = system.actorOf(Props(
+        new PersistentTaxiTripCost(aPersistentCostStatsAggregatorActor) {
+        override def persistenceId: String = "persistent-taxi-trip-cost-test-id"
+      }))
 
-      val taxiTripCost: TaxiTripCost = TaxiTripCost(vendorID = 1, tripDistance = 1.53, paymentType = 2,
+      // Auxiliary variables for commands
+      val tripId: String = "SADFASDF2886J865DASJKANDS45676IWQEWFLWF234LQHJW"
+      val createInfo: TaxiTripCost = TaxiTripCost(vendorID = 1, tripDistance = 1.53, paymentType = 2,
         fareAmount = 9, extra = 0, mtaTax = 0, tipAmount = 0, tollsAmount = 0, improvementSurcharge = 0,
         totalAmount = 2.0)
+      val updateInfo: TaxiTripCost = TaxiTripCost(vendorID = 2, tripDistance = 2.53, paymentType = 3,
+        fareAmount = 19, extra = 0.5, mtaTax = 1, tipAmount = 1, tollsAmount = 2, improvementSurcharge = 5,
+        totalAmount = 3.0)
 
-      val createCommand = TaxiTripCostCommand.CreateTaxiTripCost(tripId, taxiTripCost)
-      APersistentTaxiTripCostActor ! createCommand
+      // Create command
+      val createCommand = TaxiTripCostCommand.CreateTaxiTripCost(tripId, createInfo)
+      // Update command
+      val updateCommand = TaxiTripCostCommand.UpdateTaxiTripCost(tripId, updateInfo)
+      // Delete command
+      val deleteCommand = TaxiTripCostCommand.DeleteTaxiTripCost(tripId)
+
+      // Get command
+      val getCommand = TaxiTripCostCommand.GetTaxiTripCost(tripId)
+
+      // Sending create command
+      aPersistentTaxiTripCostActor ! createCommand
+      expectMsg(OperationResponse(tripId, Right("Success")))
+
+      // Sending update command
+      aPersistentTaxiTripCostActor ! updateCommand
+      expectMsg(OperationResponse(tripId, Right("Success")))
+
+      // Sending delete command
+      aPersistentTaxiTripCostActor ! deleteCommand
       expectMsg(OperationResponse(tripId, Right("Success")))
 
       When("A restart happens")
-      APersistentTaxiTripCostActor ! Kill
+      aPersistentTaxiTripCostActor ! Kill
 
       Then("PersistentTaxiExtraInfo should restore his state")
-      val ARestartedPersistentTaxiTripCostActor = system.actorOf(PersistentTaxiTripCost.props
-      (APersistentCostStatsAggregatorActor))
+      val aRestartedPersistentTaxiTripCostActor = system.actorOf(Props(
+        new PersistentTaxiTripCost(aPersistentCostStatsAggregatorActor) {
+          override def persistenceId: String = "persistent-taxi-trip-cost-test-id"
+        }))
 
-      val getCommand = TaxiTripCostCommand.GetTaxiTripCost(tripId)
-      ARestartedPersistentTaxiTripCostActor ! getCommand
-      expectMsg(taxiTripCost)
+      aRestartedPersistentTaxiTripCostActor ! getCommand
+      expectMsg(updateInfo.copy(deletedFlag = true))
     }
   }
 
--- src/test/scala/com/tudux/taxi/actors/extrainfo/PersistentTaxiExtraInfoSpec.scala
+++ src/test/scala/com/tudux/taxi/actors/extrainfo/PersistentTaxiExtraInfoSpec.scala
@@ -1,7 +1,7 @@
 package com.tudux.taxi.actors.extrainfo
 
-import akka.actor.{ActorRef, ActorSystem, Kill, PoisonPill}
-import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import akka.actor.{ActorRef, ActorSystem, Kill, Props}
+import akka.testkit.{ImplicitSender, TestKit}
 import com.tudux.taxi.actors.common.response.CommonOperationResponse.OperationResponse
 import org.scalatest.GivenWhenThen
 import org.scalatest.featurespec.AnyFeatureSpecLike
@@ -9,7 +9,7 @@ import org.scalatest.matchers.should.Matchers
 
 class PersistentTaxiExtraInfoSpec
   extends TestKit(ActorSystem("PersistentTaxiExtraInfoSpec"))
-  with AnyFeatureSpecLike with GivenWhenThen with ImplicitSender with Matchers {
+    with AnyFeatureSpecLike with GivenWhenThen with ImplicitSender with Matchers {
 
   info("As a external actor")
   info("I should be able to handle all the basic operations")
@@ -29,35 +29,59 @@ class PersistentTaxiExtraInfoSpec
       APersistentTaxiExtraInfoActor ! createCommand
 
       Then("response and state should correspond")
-      expectMsg(OperationResponse(tripId,Right("Success")))
+      expectMsg(OperationResponse(tripId, Right("Success")))
     }
   }
 
   Feature("handle recover") {
     Scenario("Recover state after a restart happens") {
       Given("a PersistentTaxiExtraInfo actor and commands")
-      val tripId: String = "SADFASDF2886J865DASJKANDS45676IWQEWFLWF234LQHJW"
-      val APersistentTaxiExtraInfoActor = system.actorOf(PersistentTaxiExtraInfo.props)
+      // Creating Actor
+      val aPersistentTaxiExtraInfoActor = system.actorOf(Props(new PersistentTaxiExtraInfo {
+        override def persistenceId: String = "persistent-taxi-extra-info-test-id"
+      }))
 
-      val taxiExtraInfoStat: TaxiTripExtraInfo = TaxiTripExtraInfo(pickupLongitude = -73.991127014160156,
+      // Auxiliary variables for commands
+      val tripId: String = "SADFASDF2886J865DASJKANDS45676IWQEWFLWF234LQHJW"
+      val createInfo: TaxiTripExtraInfo = TaxiTripExtraInfo(pickupLongitude = -73.991127014160156,
         pickupLatitude = 40.750080108642578, rateCodeID = 1, storeAndFwdFlag = "N", dropoffLongitude =
           -73.988609313964844, dropoffLatitude = 40.734889984130859)
-      val createCommand = TaxiTripExtraInfoCommand.CreateTaxiTripExtraInfo(tripId, taxiExtraInfoStat)
+      val updateInfo: TaxiTripExtraInfo = TaxiTripExtraInfo(pickupLongitude = 73.991127014160156,
+        pickupLatitude = -40.750080108642578, rateCodeID = 2, storeAndFwdFlag = "Y", dropoffLongitude =
+          -74.988609313964844, dropoffLatitude = 46.734889984130859)
+
+      // Create command
+      val createCommand = TaxiTripExtraInfoCommand.CreateTaxiTripExtraInfo(tripId, createInfo)
+      // Update command
+      val updateCommand = TaxiTripExtraInfoCommand.UpdateTaxiTripExtraInfo(tripId, updateInfo)
+      // Delete command
+      val deleteCommand = TaxiTripExtraInfoCommand.DeleteTaxiTripExtraInfo(tripId)
+
+      // Get command
       val getCommand = TaxiTripExtraInfoCommand.GetTaxiTripExtraInfo(tripId)
 
-      APersistentTaxiExtraInfoActor ! createCommand
-      expectMsg(OperationResponse(tripId,Right("Success")))
-      //APersistentTaxiExtraInfoActor ! getCommand
-      //expectMsg(taxiExtraInfoStat)
+      // Sending create command
+      aPersistentTaxiExtraInfoActor ! createCommand
+      expectMsg(OperationResponse(tripId, Right("Success")))
+
+      // Sending update command
+      aPersistentTaxiExtraInfoActor ! updateCommand
+      expectMsg(OperationResponse(tripId, Right("Success")))
+
+      // Sending delete command
+      aPersistentTaxiExtraInfoActor ! deleteCommand
+      expectMsg(OperationResponse(tripId, Right("Success")))
 
       When("A restart happens")
-      APersistentTaxiExtraInfoActor ! Kill
+      aPersistentTaxiExtraInfoActor ! Kill
 
       Then("PersistentTaxiExtraInfo should restore his state")
-      val ARestartedPersistentTaxiExtraInfoActor = system.actorOf(PersistentTaxiExtraInfo.props)
+      val aRestartedPersistentTaxiExtraInfoActor = system.actorOf(Props(new PersistentTaxiExtraInfo {
+        override def persistenceId: String = "persistent-taxi-extra-info-test-id"
+      }))
 
-      ARestartedPersistentTaxiExtraInfoActor ! getCommand
-      expectMsg(taxiExtraInfoStat)
+      aRestartedPersistentTaxiExtraInfoActor ! getCommand
+      expectMsg(updateInfo.copy(deletedFlag = true))
     }
   }
 

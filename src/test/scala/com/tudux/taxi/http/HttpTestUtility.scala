2fb7651ce9f4d22ba50ae797cd07f186b027c639
Víctor Gómez <vgomez@applaudostudios.com>
2022-12-08 11:45:37 -0400

    Formatter HttpTestUtility

--- src/test/scala/com/tudux/taxi/http/HttpTestUtility.scala
+++ src/test/scala/com/tudux/taxi/http/HttpTestUtility.scala
@@ -5,8 +5,7 @@ import spray.json.DefaultJsonProtocol
 object HttpTestUtility {
   case class CreateTaxiTripRequest(vendorID: Int, tpepPickupDatetime: String, tpepDropoffDatetime: String,
     passengerCount: Int, tripDistance: Double, pickupLongitude: Double, pickupLatitude: Double,
-    rateCodeID: Int,
-    storeAndFwdFlag: String, dropoffLongitude: Double, dropoffLatitude: Double,
+    rateCodeID: Int, storeAndFwdFlag: String, dropoffLongitude: Double, dropoffLatitude: Double,
     paymentType: Int, fareAmount: Double, extra: Double, mtaTax: Double,
     tipAmount: Double, tollsAmount: Double, improvementSurcharge: Double, totalAmount: Double)
 
